262a56af853e2531956d70d37b5ee51c63d5f790
A-Hilaly <hilalyamine@gmail.com>
2018-08-28 11:06:18 +0200

    fix initialize_compute instance script

--- vm-setup/initialize_compute.sh
+++ vm-setup/initialize_compute.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-apt upgrade -y
-apt-get upgrade -y
+echo "upgrading machine ..."
+echo ""
 
-apt-get install -y \
+sudo apt update -y
+sudo apt-get update -y
+sudo apt upgrade -y
+sudo apt-get upgrade -y
+sudo apt-get install -y \
     autoconf \
     automake \
     libtool \
@@ -13,11 +17,21 @@ apt-get install -y \
     python3-pip \
     curl 
 
+ubuntu_version="$1"
+
+if [ "$ubuntu_version" = "" ]; then
+    echo "settings default ubuntu version: 17.04"
+    ubuntu_version="17.04"
+fi 
+
+echo "ubuntu version: $ubuntu_version"
+echo ""
+
 function makeCUDA() {
     if [ "$ubuntu_version" = "17.04" ]; then 
         if ! dpkg-query -W cuda-9-0; then
             curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
-            dpkg -i ./cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
+            sudo dpkg -i ./cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
             sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/7fa2af80.pub
             sudo apt-get update
             sudo apt-get install cuda-9-0 -y
@@ -25,13 +39,24 @@ function makeCUDA() {
     elif [ "$ubuntu_version" = "16.04" ]; then
         if ! dpkg-query -W cuda-9-0; then
             curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-            dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+            sudo dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
             sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+            sudo apt-get update
+            sudo apt-get install cuda-9-0 -y
+        fi
+    elif [ "$ubuntu_version" = "18.04" ]; then
+        if ! dpkg-query -W cuda-9-0; then
+            curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_9.0.176-1_amd64.deb
+            sudo dpkg -i ./cuda-repo-ubuntu1804_9.0.176-1_amd64.deb
+            sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
             sudo apt-get update
             sudo apt-get install cuda-9-0 -y
         fi
     fi
 }
+
+echo "installing cuda..."
+echo ""
 
 makeCUDA
 
@@ -40,14 +65,12 @@ nvidia-smi -pm 1
 
 function makeCUDNN() {
     # download cudnn
-    gsutil cp gs://medtune/cudnn/cudnn-9.0-linux-x64-v7.1.tar .
-
+    sudo gsutil cp gs://medtune/cudnn/cudnn-9.0-linux-x64-v7.1.tar .
     # install cudnn
-    tar -xzvf cudnn-9.0-linux-x64-v7.1.tar
+    tar -xvf cudnn-9.0-linux-x64-v7.1.tar
     sudo cp cuda/include/cudnn.h /usr/local/cuda/include
     sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
     sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-
     # clean
     rm cudnn-9.0-linux-x64-v7.1.tar
     rm -rf cudnn
@@ -55,6 +78,7 @@ function makeCUDNN() {
 
 makeCUDNN
 
+echo "" >> $HOME/.profile
 echo "export CUDA_HOME=/usr/local/cuda" >> $HOME/.profile
 echo "export PATH=$PATH:$CUDA_HOME/bin" >> $HOME/.profile
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64" >> $HOME/.profile
